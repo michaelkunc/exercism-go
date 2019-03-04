bc18e923241950d9479c8d87c89922770a570316
michaelkunc <mjkunc@gmail.com>
2019-03-04 13:09:18 -0800

    start of bob

---
+++ bob/bob.go
@@ -0,0 +1,22 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package bob should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package bob
+
+import "strings"
+
+// Hey should have a comment documenting it.
+func Hey(remark string) string {
+	if remark == strings.ToUpper(remark) && strings.HasSuffix(remark, "?") {
+		return "Calm down, I know what I'm doing!"
+	} else if remark == strings.ToUpper(remark) {
+		return "Whoa, chill out!"
+	} else if strings.HasSuffix(remark, "?") {
+		return "Sure."
+	} else {
+		return "Whatever."
+
+	}
+}
