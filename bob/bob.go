13fe196a2188365878cebca59502cbd1dc0db97b
michaelkunc <mjkunc@gmail.com>
2019-03-05 20:27:50 -0800

    completed bob

--- bob/bob.go
+++ bob/bob.go
@@ -5,32 +5,30 @@
 // https://golang.org/doc/effective_go.html#commentary
 package bob
 
-import "strings"
-
-const letters = "abcdefghijklmnopqrstuvwxyz"
+import (
+	"regexp"
+	"strings"
+)
 
 // Hey should have a comment documenting it.
 func Hey(remark string) string {
-	isUpper := strings.ToUpper(remark) == remark
-	isQuestion := strings.HasSuffix(remark, "?")
-	isExclamation := strings.HasSuffix(remark, "!")
+	trimRemark := strings.TrimSpace(remark)
+	isUpper := strings.ToUpper(trimRemark) == trimRemark
+	isQuestion := strings.HasSuffix(trimRemark, "?")
+	isExclamation := strings.HasSuffix(trimRemark, "!")
+	anyLettersRe, _ := regexp.Compile("[a-zA-Z]")
+	anyLetters := anyLettersRe.MatchString(trimRemark)
+	isSilence := len(trimRemark) == 0
 	switch {
-	case isUpper && isQuestion && onlyLetters(remark):
+	case isUpper && isQuestion && anyLetters:
 		return "Calm down, I know what I'm doing!"
-	case (isUpper && isExclamation) || (isUpper && onlyLetters(remark)):
+	case (isUpper && isExclamation) || (isUpper && anyLetters):
 		return "Whoa, chill out!"
 	case isQuestion:
 		return "Sure."
+	case isSilence:
+		return "Fine. Be that way!"
 	default:
 		return "Whatever."
 	}
-}
-
-func onlyLetters(phrase string) bool {
-	for _, char := range phrase {
-		if !strings.Contains(letters, strings.ToLower(string(char))) {
-			return false
-		}
-	}
-	return true
 }
