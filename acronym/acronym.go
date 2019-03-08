9d805db6ffe4afd6472535a110c407ca8bce6cfe
michaelkunc <mjkunc@gmail.com>
2019-03-08 06:07:55 -0800

    acronym solution

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -6,9 +6,10 @@ import "strings"
 // Abbreviate should have a comment documenting it.
 func Abbreviate(s string) string {
 	var letters []string
-	wordList := strings.Split(s, " ")
+	wordList := strings.Fields(strings.ReplaceAll(s, "-", " "))
 	for _, v := range wordList {
-		letters = append(letters, string(v[0]))
+		firstLetter := string(v[0])
+		letters = append(letters, strings.ToUpper(firstLetter))
 	}
 	return strings.Join(letters, "")
 
