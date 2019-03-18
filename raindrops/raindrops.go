527e8286c2c1329e748181a255c1ca25ea6c76d4
michaelkunc <mjkunc@gmail.com>
2019-03-18 08:51:45 -0700

    raindrops in process

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,14 +1,24 @@
 // Package raindrops is for the raindrops exercise
 package raindrops
 
-// Convert does the coversion
-func Convert(n int) string {
-	var result string
-	if n%3 == 0 {
-		result += "Pling"
-	} else {
-		result += string(n)
-	}
+import "strconv"
 
+// Convert does the coversion
+func Convert(n int) (result string) {
+	var dropMapping = map[int]string{
+		3: "Pling",
+		5: "Plang",
+		7: "Plong",
+	}
+	for num, noise := range dropMapping {
+		if n%num == 0 {
+			n /= num
+			result += noise
+
+		}
+	}
+	if result == "" {
+		result = strconv.Itoa(n)
+	}
 	return result
 }
